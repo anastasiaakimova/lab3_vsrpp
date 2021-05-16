db30817525e4d344fee6fa2a5b0f724fd0a55277
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-05-16 21:31:40 +0400

    add faculty script

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -34,3 +34,21 @@ INSERT INTO `university`.`students` (`id`, `name`, `surname`, `phone_number`, `g
 VALUES ('2', 'dsdfs', 'rthyhy', '231456', '1');
 INSERT INTO `university`.`students` (`id`, `name`, `surname`, `phone_number`, `group_id`)
 VALUES ('3', 'sdfg', 'thty6tf', '1234567890', '2');
+
+
+CREATE TABLE `university`.`faculty`
+(
+    `faculty_id`           INT          NOT NULL AUTO_INCREMENT,
+    `name`         VARCHAR(255) NULL,
+    `phone_number` VARCHAR(20)  NULL,
+    `group_id`     INT          NULL,
+    PRIMARY KEY (`faculty_id`),
+    CONSTRAINT `fk`
+        FOREIGN KEY (`group_id`)
+            REFERENCES `university`.`groups` (`group_id`)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+);
+
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `phone_number`, `group_id`) VALUES ('1', 'a', '3445', '1');
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `phone_number`, `group_id`) VALUES ('2', 'b', '34567', '2');
